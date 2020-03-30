9b865c0566bb6a8a1d9decf11fa79d1e622eb8e4
Finta Gabor <fintagabor8@gmail.com>
2020-03-30 07:37:18 +0200

    Fix wrong function name

--- firebase_main.js
+++ firebase_main.js
@@ -2,4 +2,4 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const app = require('./app');
 
-exports.cause = functions.https.onRequest(app);
+exports.user = functions.https.onRequest(app);
