c172f1e3c0ad3a2c716f770c5cb46d58a24bebe7
Levente Fodor <levilevi183@gmail.com>
2020-04-05 00:19:41 +0200

    Add cors package

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const createRouter = require('./src/router');
+const cors = require('cors');
 
 function createApp(datastore) {
     app = express();
+    app.use(cors())
     app.use(express.json());
     app.use('/', createRouter(datastore.userStore));
     return app;
