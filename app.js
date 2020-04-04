425becbf46e693a67579da9cf9539d45d45e4d23
Levente Fodor <levilevi183@gmail.com>
2020-04-05 01:05:57 +0200

    configure cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const createRouter = require('./src/router');
-const cors = require('cors');
+const cors = require('cors')({origin: true});
 
 function createApp(datastore) {
     app = express();
