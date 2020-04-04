fc7421efd22562329eff9abf8067d2770f2bcc86
Nagy Bálint <nagybalint000@gmail.com>
2020-04-05 01:11:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const createRouter = require('./src/router');
-const cors = require('cors')({origin: true});
+const cors = require('cors');
 
 function createApp(datastore) {
     app = express();
-    app.use(cors())
+    app.use(cors({origin: true}));
     app.use(express.json());
     app.use('/', createRouter(datastore.userStore));
     return app;
